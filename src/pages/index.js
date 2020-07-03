8ccaad4cb4e4043f69f87eee17b49659f6b4cb23
Nicolas Zwierzykowski <nicolas@softniels.com.br>
2020-07-03 18:05:10 -0300

    new: last posts section on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import Profile from "../components/Profile";
+import LastPosts from "../components/LastPosts";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Profile />
+    <LastPosts />
   </Layout>
 )
 
