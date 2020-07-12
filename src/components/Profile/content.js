11ba664ead9214f49502c223e2a2c611796f4d30
Nicolas Zwierzykowski <nicolas@softniels.com.br>
2020-07-12 17:56:42 -0300

    new profile description

--- src/components/Profile/content.js
+++ src/components/Profile/content.js
@@ -2,7 +2,16 @@ export const profile = {
     name: `Nicolas Zwierzykowski`,
     briefDescription: `Front-End Developer`,
     longDescription: [
-        `Hi, I'm a full - stack web developer who loves building awesome software and apps. I'm obsessed with the idea of ​​using software to solve all kinds of problems and help people. Talking about my specialties, I love to learn every day and I quickly learn new skills and programming languages, tests, problem-solving, and software architecture as well.`,
-        `So far I have HTML, CSS, Javascript, AngularJS, React, Git, Python, Delphi, SQL, and TDD. I've started learning data structures and algorithms.`,
+        `Olá, meu nome é Nicolas e eu sou Desenvolvedor Front-end, sou formado em Sistemas de Informação e atualmente trabalho na Softniel's, em União da Vitória/PR. Eu gosto muito da ideia de usar software para resolver todos os tipos de problemas e facilitar a vida de pessoas com isso. 
+        Gosto muito de aprender todos os dias e facilmente aprendo novas habilidades.`,
+        `Até o momento eu sei HTML, CSS, Javascript, React, AngularJS, Git, SQL, testes, TDD, Python e Delphi. Também estou aprendendo sobre estruturas de dados e algoritmos.`,
     ]
+    // longDescription: [
+    //     `Hi, I'm a full - stack web developer who loves building awesome software and apps. 
+    //      I'm obsessed with the idea of ​​using software to solve all kinds of problems and help people. 
+    //       Talking about my specialties, I love to learn every day and I quickly learn new skills and programming languages, tests,
+    //      problem-solving, and software architecture as well.`,
+    //     `So far I have HTML, CSS, Javascript, AngularJS, React, Git, Python, Delphi, SQL, and TDD. 
+    //      I've started learning data structures and algorithms.`,
+    // ]
 };
