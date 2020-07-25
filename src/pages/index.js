14e1d07abde7d9597b2d05af852cbcf1b13fa256
Nicolas Zwierzykowski <nicolas@softniels.com.br>
2020-07-25 13:49:37 -0300

    Removed unused import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import Profile from "../components/Profile";
