e37958da4fb0ea0e9d391b928ada46e18c041add
Nicolas Zwierzykowski <nicolas@softniels.com.br>
2020-10-03 18:34:59 -0300

    added english description to profile page

--- src/components/Profile/content.js
+++ src/components/Profile/content.js
@@ -1,17 +1,21 @@
 export const profile = {
     name: `Nicolas Zwierzykowski`,
-    briefDescription: `Front-End Developer`,
+    briefDescription: `Frontend developer`,
     longDescription: [
         `Olá, meu nome é Nicolas e eu sou Desenvolvedor Front-end, sou formado em Sistemas de Informação e atualmente trabalho na Softniel's, em União da Vitória/PR. Eu gosto muito da ideia de usar software para resolver todos os tipos de problemas e facilitar a vida de pessoas com isso. 
         Gosto muito de aprender todos os dias e facilmente aprendo novas habilidades.`,
         `Até o momento eu sei HTML, CSS, Javascript, React, AngularJS, Git, SQL, testes, TDD, Python e Delphi. Também estou aprendendo sobre estruturas de dados e algoritmos.`,
     ]
-    // longDescription: [
-    //     `Hi, I'm a full - stack web developer who loves building awesome software and apps. 
-    //      I'm obsessed with the idea of ​​using software to solve all kinds of problems and help people. 
-    //       Talking about my specialties, I love to learn every day and I quickly learn new skills and programming languages, tests,
-    //      problem-solving, and software architecture as well.`,
-    //     `So far I have HTML, CSS, Javascript, AngularJS, React, Git, Python, Delphi, SQL, and TDD. 
-    //      I've started learning data structures and algorithms.`,
-    // ]
+};
+
+export const profileEnglish = {
+    name: `Nicolas Zwierzykowski`,
+    briefDescription: `Software engineer`,
+    longDescription: [
+        `Hi, my name is Nicolas and I'm a Software engineer, 
+        I have a degree in Information Systems and currently I work at Softniel's, in União da Vitória/PR - Brazil. 
+        I'm obsessed with the idea of ​​using software to solve all kinds of problems and make people's life easier with that. 
+        I love to learn every day and I quickly learn new skills.`,
+        `So far I have HTML, CSS, Javascript, React, AngularJS, Git, SQL, tests, TDD, Python and Delphi. Also, I've started learning data structures and algorithms.`,
+    ]
 };
