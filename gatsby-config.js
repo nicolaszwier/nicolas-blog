44d9c90db06b8a6b4a6fda01818325bced090eba
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2022-11-09 17:46:05 -0300

    update google analytics plugin to new gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,9 +93,25 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "UA-172427936-1",
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-172427936-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Defaults to https://www.googletagmanager.com
+          origin: "nicolasz.dev",
+        },
       },
     },
     `gatsby-plugin-offline`,
