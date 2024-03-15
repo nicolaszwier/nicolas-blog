f681a3e535763c7a647cac9318bf5c98fc35bca5
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-03-15 16:20:48 -0400

    avatar sizing fix

--- src/components/Profile/content.js
+++ src/components/Profile/content.js
@@ -1,8 +1,8 @@
 export const profile = {
     name: `Nicolas Zwierzykowski`,
-    briefDescription: `Frontend developer`,
+    briefDescription: `Software Engineer`,
     longDescription: [
-        `Olá, sou um dedicado Engenheiro de Software com amplo conhecimento em desenvolvimento Frontend. Sempre estou buscando aprender coisas novas, o que me permite adquirir novas habilidades com destreza. Possuo bom conhecimento em areas relacionadas ao desenvolvimento de software, incluindo testes, resolução de problemas e arquitetura de software.
+        `Olá, sou um Engenheiro de Software com amplo conhecimento em desenvolvimento Frontend. Sempre estou buscando aprender coisas novas, o que me permite adquirir novas habilidades com destreza. Possuo bom conhecimento em areas relacionadas ao desenvolvimento de software, incluindo testes, resolução de problemas e arquitetura de software.
         Como especialista em Frontend, sou proficiente na criação e desenvolvimento de interfaces de usuário que proporcionam experiências excepcionais aos usuários. Além disso, possuo a versatilidade necessária para contribuir com o desenvolvimento Backend quando necessário.`,
         `Minhas habilidades de programação abrangem uma ampla gama, incluindo Javascript, Typescript, Python, MySQL e Postgres. Tenho ampla experiência em frameworks-chave, como React, NextJs, Angular 2+, Flask, NodeJS, bem como ferramentas como JEST e GatsbyJS. Além disso, tenho uma boa compreensão dos elementos essenciais da web, incluindo HTML, CSS, acessibilidade, Webpack e Contentful.
         Esse conjunto de habilidades, combinado com meu esforço diário, me posiciona como um Engenheiro de Software altamente capaz e adaptável, preparado para se destacar em um ambiente profissional dinâmico e desafiador.`
@@ -13,7 +13,7 @@ export const profileEnglish = {
     name: `Nicolas Zwierzykowski`,
     briefDescription: `Software engineer`,
     longDescription: [
-        `Hey there, I am a dedicated Software Engineer with a strong knowledge of Frontend development. My professional profile is defined by a profound commitment to continuous learning, enabling me to adeptly acquire new skills. I excel in various aspects of software development, including testing, problem-solving, and software architecture.
+        `Hey there, I am a Software Engineer with a strong knowledge of Frontend development. My professional profile is defined by a profound commitment to continuous learning, enabling me to adeptly acquire new skills. I excel in various aspects of software development, including testing, problem-solving, and software architecture.
         As a Frontend Specialist, I am proficient in designing and developing user interfaces that offer exceptional user experiences. Additionally, I possess the versatility to contribute to Backend development as necessary.`,
         `My programming skills encompass a broad spectrum, encompassing Javascript, Typescript, Python, MySQL, and Postgres. I have extensive experience with key frameworks such as React, NextJs, Angular 2+, Flask, NodeJS and tools like JEST, and GatsbyJS. Furthermore, I have a comprehensive understanding of web essentials, including HTML, CSS, accessibility, Webpack, and Contentful.
         This skill set, combined with my commitment to excellence, positions me as a highly capable and adaptable Software Engineer, prepared to excel in a dynamic and challenging professional environment.`
