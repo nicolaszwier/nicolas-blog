0e1bc882f0006b50443c0a612f9929ec5e01b801
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-10-07 16:07:55 -0400

    feat: added new file for the content i18n

---
+++ src/data/i18n.js
@@ -0,0 +1,153 @@
+export const profile = {
+  en: {
+    name: `Nicolas Zwierzykowski`,
+    briefDescription: `Software engineer`,
+    longDescription: [
+        `Software Engineer with 6+ years of experience in web development, specializing in scalable, high-quality applications. I have contributed to various projects across multiple domains. 
+        I’ve recently been working in the telecommunication sector where I have played a key role in delivering high-quality and scalable products that offer great user experiences and are utilized by thousands of users daily, 
+        generating significant revenue for the company.`,
+        `Specialized in Frontend and Backend development, with experience in technologies such as Typescript, Angular, React, NodeJs, SQL, MongoDB, among others.`
+    ],
+    seeMore: 'See more',
+    languageToggle: 'Acessar site em português'
+  },
+  'pt-br': {
+    name: `Nicolas Zwierzykowski`,
+    briefDescription: `Software Engineer`,
+    longDescription: [
+        `Engenheiro de Software com mais de 6 anos de experiência em desenvolvimento web, com excelência em tecnologias frontend como Angular e React.
+        Com experiência em diversos ramos, mais recentemente no setor de telecomunicações, onde desempenhei um papel fundamental na entrega de produtos de alta qualidade e escaláveis, que proporcionam ótimas experiências aos usuários e são utilizados por milhares de pessoas diariamente,
+        gerando receita significativa para a empresa.`,
+        `Especializado em desenvolvimento Frontend e Backend, com experiência em tecnologias como Typescript, Angular, React, NodeJs, SQL, MongoDB, entre outras.`
+    ],
+    seeMore: 'Ver mais',
+   languageToggle: 'Change to english'
+  }
+};
+
+export const aboutMe = {
+  en: {
+    title: `About me`,
+    subtitle: `
+      <p>
+        I am a <strong>Software Engineer</strong> with over <strong>6 years of experience</strong> in 
+        <strong>web development</strong>, specializing in building <strong>scalable, high-performance applications</strong> that 
+        deliver exceptional user experiences. Throughout my career, I have contributed to a wide range of projects 
+        across various industries, from startups to established enterprises.
+      </p>
+      
+      <p>
+        I have played a pivotal role in creating <strong>high-quality, reliable products</strong> used by 
+        <strong>thousands of users</strong> daily, helping businesses drive user engagement and generate significant 
+        revenue through effective digital solutions. My work reflects the importance of well-designed, scalable 
+        applications that meet both user needs and business goals.
+      </p>
+      
+      <p>
+        My strengths lie in <strong>Fullstack development</strong>, with expertise in modern <strong>Frontend frameworks</strong>, 
+        efficient <strong>back-end architectures</strong>, and <strong>cloud-based services</strong> that ensure long-term scalability. 
+        I am passionate about <strong>solving complex problems</strong>, continuously improving <strong>code quality</strong>, and 
+        staying up-to-date with the latest trends in web development to deliver forward-thinking, future-proof solutions.
+      </p>
+  
+    `,
+    // subtitle: `
+    // <p>Software Engineer with 6+ years of experience in web development, specializing in scalable, high-quality applications. I have contributed to various projects across multiple domains. 
+    //     I’ve recently been working in the telecommunication sector where I have played a key role in delivering high-quality and scalable products that offer great user experiences and are utilized by thousands of users daily, 
+    //     generating significant revenue for the company.</p>
+    // `,
+    experience: 'See my full professional experience',
+    projects: 'See the projects I’m working on',
+    skills: 'My skills',
+    programmingLanguages: {
+      label: 'Programming languages',
+      stack: ['Javascript', 'Typescript', 'Swift', 'HTML', 'CSS']
+    },
+    frameworks: {
+      label: 'Frameworks/Libraries',
+      stack: ['Angular', 'ReactJS', 'Node.js', 'Next.js', 'Nest.js', 'SwiftUI', 'Styled Components', 'MaterialUI']
+    }, 
+    databases: {
+      label: 'Databases',
+      stack: ['MongoDB', 'MySQL', 'PostgreSQL']
+    }, 
+    cloud: {
+      label: 'Cloud platforms',
+      stack: ['AWS', 'Azure']
+    }, 
+    other: {
+      label: 'Other technologies',
+      stack: ['Git','Accessibility', 'ContentfulCMS', 'Tailwind CSS', 'Sass']
+    }, 
+    testing: {
+      label: 'Testing tools',
+      stack: ['Jest', 'Jasmine', 'Unit testing']
+    }, 
+    agile: {
+      label: 'Project management/Agile',
+      stack: ['Scrum', 'Kanban', 'Jira']
+    }, 
+    footnoteHtml: `HTML: although a markup language, it's closely associated with programming for web development`,
+    footnoteCss: `CSS: style sheet language, but often categorized alongside programming languages for web development`,
+  },
+  'pt-br': {
+    title: `Sobre mim`,
+    subtitle: `
+      <p>
+        Sou um <strong>Engenheiro de Software</strong> com mais de <strong>6 anos de experiência</strong> em 
+        <strong>desenvolvimento web</strong>, especializado na construção de <strong>aplicações escaláveis e de alto desempenho</strong> que 
+        proporcionam experiências excepcionais aos usuários. Ao longo da minha carreira, contribuí para uma ampla gama de projetos 
+        em diversos setores, desde startups até multinacionais consolidadas.
+      </p>
+      
+      <p>
+        Desempenhei um papel fundamental na criação de <strong>produtos confiáveis e de alta qualidade</strong> utilizados por 
+        <strong>milhares de usuários</strong> diariamente, ajudando as empresas a impulsionar o engajamento dos usuários e gerar 
+        receitas significativas por meio de soluções digitais eficazes. Meu trabalho reflete a importância de aplicações bem projetadas e escaláveis 
+        que atendam tanto às necessidades dos usuários quanto aos objetivos de negócios.
+      </p>
+      
+      <p>
+        Meus pontos fortes estão no <strong>desenvolvimento Fullstack</strong>, com expertise em <strong>frameworks modernos de Frontend</strong>, 
+        arquiteturas de <strong>Backend eficientes</strong> e <strong>serviços baseados em nuvem</strong> que garantem escalabilidade a longo prazo. 
+        Sou apaixonado por <strong>resolver problemas complexos</strong>, melhorar continuamente a <strong>qualidade do código</strong> e 
+        me manter atualizado com as últimas tendências em desenvolvimento web para oferecer soluções inovadoras e preparadas para o futuro.
+      </p>
+
+
+    `,
+    experience: 'Veja minha experiência profissional completa',
+    projects: 'Veja os projetos que estou trabalhando',
+    skills: 'Tecnologias que trabalho',
+    programmingLanguages: {
+      label: 'Linguagens de programação',
+      stack: ['Javascript', 'Typescript', 'Swift', 'HTML', 'CSS']
+    },
+    frameworks: {
+      label: 'Frameworks/Bibliotecas',
+      stack: ['Angular', 'ReactJS', 'Node.js', 'Next.js', 'Nest.js', 'SwiftUI', 'Styled Components', 'MaterialUI']
+    }, 
+    databases: {
+      label: 'Banco de dados',
+      stack: ['MongoDB', 'MySQL', 'PostgreSQL']
+    }, 
+    cloud: {
+      label: 'Cloud',
+      stack: ['AWS', 'Azure']
+    }, 
+    other: {
+      label: 'Outras tecnologias',
+      stack: ['Git','Accessibility', 'ContentfulCMS', 'Tailwind CSS', 'Sass']
+    }, 
+    testing: {
+      label: 'Testes',
+      stack: ['Jest', 'Jasmine', 'Unit testing']
+    }, 
+    agile: {
+      label: 'Metodologias ágeis',
+      stack: ['Scrum', 'Kanban', 'Jira']
+    }, 
+    footnoteHtml: `HTML: embora seja uma linguagem de marcação, é frequentemente associada à programação para desenvolvimento web`,
+    footnoteCss: `CSS: linguagem de folhas de estilo, mas muitas vezes categorizada junto com linguagens de programação para desenvolvimento web`,
+  }
+}
